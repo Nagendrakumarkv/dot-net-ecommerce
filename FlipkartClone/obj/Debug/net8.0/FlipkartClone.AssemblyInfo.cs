d1f0d60bb3251d588eb4a70d8ef1728808e0bdc4
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2025-12-16 14:59:19 +0530

    Day 3: Base Layout Design (Navbar, Footer, Bootstrap setup for that "Flipkart Blue" look).

--- FlipkartClone/obj/Debug/net8.0/FlipkartClone.AssemblyInfo.cs
+++ FlipkartClone/obj/Debug/net8.0/FlipkartClone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlipkartClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6df631cc61ac2a6097d8f2a5c4d59bfc5278d5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8099905e9b0c2fed40617e1c388b6969fe5e23b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlipkartClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlipkartClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
